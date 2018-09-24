76730c5ffc472187b41291f45081974b00ef8cd6
Павел <43180298+PavelSab@users.noreply.github.com>
2018-09-24 23:38:52 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation=equation.replace(/\s/g, '');
+  var array=[];
+  var indexA=equation.search(/\^/);
+  var a=parseInt(equation.slice(0,indexA));
+  var indexB=equation.search(/\*x[+-]/);
+  var b=parseInt(equation.slice(indexA+2,indexB));
+  var c=parseInt(equation.slice(indexB+2));
+
+  var discremenant=(b*b)-(4*a*c);
+      var x1=Math.round((-b+Math.sqrt(discremenant))/(2*a));
+      var x2=Math.round((-b-Math.sqrt(discremenant))/(2*a));
+
+      array.push(x1,x2);
+      return array.sort(function (a,b) {
+          return a-b;
+      });
+
 }
